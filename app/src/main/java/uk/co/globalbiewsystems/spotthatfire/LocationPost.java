4291ac3493616aedd51b16d0bb8c3d1fd50b794a
Sam Mercer <sam.mercer@globalviewsysems.co.uk>
2018-10-20 12:49:16 +0100

    Post Data

--- app/src/main/java/uk/co/globalbiewsystems/spotthatfire/LocationPost.java
+++ app/src/main/java/uk/co/globalbiewsystems/spotthatfire/LocationPost.java
@@ -1,9 +1,15 @@
 package uk.co.globalbiewsystems.spotthatfire;
 
-import android.util.Log;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LocationPost {
     public void postData(String Url, String body){ MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -14,20 +20,19 @@ public class LocationPost {
                 .readTimeout(30, TimeUnit.SECONDS)
                 .build();
         final Request request = new Request.Builder()
-                .addHeader("Authorization", "bearer " + authToken)
+
                 .url(Url)
                 .post(requestBody)
                 .build();
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("API Post", "Failed to send data to API!");
-                postDataCallback.onFailure();
+
             }
+
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d("API Post", "Data sent to API!");
-                postDataCallback.onSuccess();
+
             }
         });
     }
